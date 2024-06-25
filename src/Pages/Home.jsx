5f9cfef335cc9fe773f5e8550f21e61f14e8bc05
nareshselvan6 <nareshselvan6@gmail.com>
2024-06-25 15:38:08 +0530

    small touchup updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,18 +16,11 @@ const Home = () => {
       <div className="totalhome home">
         <div> 
 
-        <h1 className="titlehome d-flex flex-flex-wrap row-cols-md-10 row-cols-sm-8">
+        <h1 className="titlehome  flex-flex-wrap row-cols-md-10 row-cols-sm-8">
           Welcome to <span className="hometitlespan">S</span> Carcare
         </h1>
         </div>
 
-        <br />
-
-     
-     <div>
-      <h1>hllo</h1>
-     </div>
-     
         <br />
       </div>
     </div>
