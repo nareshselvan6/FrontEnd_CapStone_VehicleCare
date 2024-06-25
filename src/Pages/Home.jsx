74a9038603185c1a70aa98641d570e5c733efe62
nareshselvan6 <nareshselvan6@gmail.com>
2024-06-25 15:09:36 +0530

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -21,52 +21,9 @@ const Home = () => {
         </h1>
         </div>
 
-        {/* <span className="hometitlespanS ts">S</span>  */}
         <br />
 
-        <div className="firstpic">
-        <Carousel data-bs-theme="dark">
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="images\washingpics\washingpic1.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h5>First slide label</h5>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="images\washingpics\washingpic2.jpg"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h5>Second slide label</h5>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="images\washingpics\washingpic3.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h5>Third slide label</h5>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-
-
-
-
-     </div>
+     
      <div>
       <h1>hllo</h1>
      </div>
