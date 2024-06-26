90241944eb3dd0a203c851bca593abefa91523fa
nareshselvan6 <nareshselvan6@gmail.com>
2024-06-26 10:38:24 +0530

    bug fix

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../Components/Components.css";
 import { Link, useNavigate } from "react-router-dom";
+import tsimg from "../../images/TS.jpg"
 
 const NavBar = () => {
 
@@ -19,7 +20,7 @@ const token=localStorage.getItem("jwt")
       <div className="totalnavbar ">
     <nav className="navbar navbar-expand-lg navbar-dark  navbar-shrink" id="mainNav">
   <div className="container-fluid">
-  <div className="tsimagediv d-flex align-items-center gap-1 "><img src="images\TS.jpg" className="tsimage " /> CarCare </div>
+  <div className="tsimagediv d-flex align-items-center gap-1 "><img src={tsimg} className="tsimage " /> CarCare </div>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       Menu
       <svg className="svg-inline--fa fa-bars ms-1" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" data-fa-i2svg><path fill="currentColor" d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" /></svg>{/* <i class="fas fa-bars ms-1"></i> Font Awesome fontawesome.com */}
